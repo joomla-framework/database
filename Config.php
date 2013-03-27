daedb25dd4113e1ddcb48fab141b17f477d3c0ff
Don Gilbert <don@dongilbert.net>
2013-03-27 00:31:19 -0500

    Move vendor/Joomla to src/Joomla. Update necessary paths.

---
+++ Config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Part of the Joomla Framework Test Package
+ *
+ * @copyright  Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Joomla\Test;
+
+/**
+ * Test stub configuration class.
+ *
+ * @since  1.0
+ */
+class Config
+{
+	public $foo = 'bar';
+}
