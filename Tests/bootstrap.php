c0069a188ba6910474601fc3faf5e428af2d932a
Michael Babker <mbabker@flbab.com>
2014-02-08 13:31:52 -0600

    Add a tests bootstrap to define JPATH_ROOT

---
+++ Tests/bootstrap.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Unit test runner bootstrap file for the Joomla Framework.
+ *
+ * @copyright  Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ * @link       http://www.phpunit.de/manual/current/en/installation.html
+ */
+
+// Fix magic quotes.
+@ini_set('magic_quotes_runtime', 0);
+
+// Maximise error reporting.
+error_reporting(E_ALL & ~E_STRICT);
+ini_set('display_errors', 1);
+
+/*
+ * Ensure that required path constants are defined.  These can be overridden within the phpunit.xml file
+ * if you chose to create a custom version of that file.
+ */
+if (!defined('JPATH_ROOT'))
+{
+	define('JPATH_ROOT', realpath(dirname(__DIR__)));
+}
+
+// Search for the Composer autoload file
+$composerAutoload = dirname(__DIR__) . '/vendor/autoload.php';
+
+if (file_exists($composerAutoload))
+{
+	include_once $composerAutoload;
+}
