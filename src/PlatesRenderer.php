c245015d3a938ead1c90a5cbc9c83d7343bf003c
Michael Babker <mbabker@flbab.com>
2014-08-22 18:58:07 -0400

    Code style to Joomla! Coding Standard

--- src/PlatesRenderer.php
+++ src/PlatesRenderer.php
@@ -10,6 +10,7 @@ namespace BabDev\Renderer;
 
 use League\Plates\Template;
 use League\Plates\Engine;
+
 /**
  * Plates class for rendering output.
  *
@@ -17,130 +18,145 @@ use League\Plates\Engine;
  */
 class PlatesRenderer extends Template implements RendererInterface
 {
-        
-    private $_engine;
-    
-    private $_config;
-    
-    private $_data = array();
+	/**
+	 * Rendering engine
+	 *
+	 * @var    Engine
+	 * @since  1.0
+	 */
+	private $engine;
 
-    /**
-     * A constructor method
-     *
-     * @param  array  $config  Configurations
-     *
-     * @return void
-     *
-     * @since   1.0
-     */
-    public function __construct($config = array())
-    {
-        $this->_config = $config;
-        $this->_engine = new Engine();
-    }
-    
-    /**
-     * Render and return compiled data.
-     *
-     * @param   string  $template  The template file name
-     * @param   array   $data      The data to pass to the template
-     *
-     * @return  string  Compiled data
-     *
-     * @since   1.0
-     */
-    public function render($template, array $data = array())
-    {
-        $data = array_merge($this->_data, $data);
-        
-        //TODO Process template name
-        
-        parent::render($template, $data);
-    }
-    
-    /**
-     * Add a folder with alias to the renderer
-     *
-     * @param  string  $alias      The folder alias
-     * @param  string  $directory  The folder path
-     * 
-     * @return  boolean  TRUE if the folder is loaded
-     *
-     * @since 1.0
-     */
-    public function addFolder($alias, $directory)
-    {
-        $this->_engine->addFolder($alias, $directory);
-    }
-    
-    /**
-     * Sets file extension for template loader
-     *
-     * @param  string  $extension  Template files extension
-     *
-     * @return  void
-     *
-     * @since 1.0
-     */
-    public function setFileExtension($extension)
-    {
-        $this->_engine->setFileExtension($extension);
-    }
-    
-    /**
-     * Checks if folder, folder alias, template or template path exists
-     *
-     * @param  string  $path  Full path or part of a path
-     *
-     * @return  boolean  TRUE of the path exists
-     *
-     * @since  1.0
-     */
-    public function pathExists($path)
-    {
-        //@TODO check for directories
-        return $this->_engine->exists($path);
-    }
-    
-    /**
-     * Loads data from array into the renderer
-     *
-     * @param  array  $data  Array of variables
-     *
-     * @return boolean  TRUE if data loaded successfully
-     *
-     * @since  1.0
-     */
-    public function setData($data)
-    {
-        $this->_data = $data; 
-    }
-    
-    /**
-     * Unloads data from renderer
-     *
-     * @return void
-     *
-     * @since  1.0
-     */
-    public function unsetData()
-    {
-        $this->_data = array();
-    }
-    
-    /**
-     * Sets a piece of data
-     *
-     * @param  string  $key    Name of variable
-     * @param  string  $value  Value of variable
-     *
-     * @return RendererInterface  Returns self for chaining
-     *
-     * @since  1.0
-     */
-    public function set($key, $value)
-    {
-        //TODO Make use of Joomla\Registry to provide paths
-        $this->_data[$key] = $value;
-    }
+	/**
+	 * Configuration array
+	 *
+	 * @var    array
+	 * @since  1.0
+	 */
+	private $config;
+
+	/**
+	 * Data for output by the renderer
+	 *
+	 * @var    array
+	 * @since  1.0
+	 */
+	private $data = array();
+
+	/**
+	 * A constructor method
+	 *
+	 * @param   array  $config  Configurations
+	 *
+	 * @since   1.0
+	 */
+	public function __construct($config = array())
+	{
+		$this->config = $config;
+		$this->engine = new Engine;
+	}
+
+	/**
+	 * Render and return compiled data.
+	 *
+	 * @param   string  $template  The template file name
+	 * @param   array   $data      The data to pass to the template
+	 *
+	 * @return  string  Compiled data
+	 *
+	 * @since   1.0
+	 */
+	public function render($template, array $data = array())
+	{
+		$data = array_merge($this->data, $data);
+
+		// TODO Process template name
+
+		parent::render($template, $data);
+	}
+
+	/**
+	 * Add a folder with alias to the renderer
+	 *
+	 * @param   string  $alias      The folder alias
+	 * @param   string  $directory  The folder path
+	 *
+	 * @return  boolean  True if the folder is loaded
+	 *
+	 * @since   1.0
+	 */
+	public function addFolder($alias, $directory)
+	{
+		$this->engine->addFolder($alias, $directory);
+	}
+
+	/**
+	 * Sets file extension for template loader
+	 *
+	 * @param   string  $extension  Template files extension
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function setFileExtension($extension)
+	{
+		$this->engine->setFileExtension($extension);
+	}
+
+	/**
+	 * Checks if folder, folder alias, template or template path exists
+	 *
+	 * @param   string  $path  Full path or part of a path
+	 *
+	 * @return  boolean  TRUE of the path exists
+	 *
+	 * @since   1.0
+	 */
+	public function pathExists($path)
+	{
+		// TODO check for directories
+		return $this->engine->exists($path);
+	}
+
+	/**
+	 * Loads data from array into the renderer
+	 *
+	 * @param   array  $data  Array of variables
+	 *
+	 * @return  boolean  True if data loaded successfully
+	 *
+	 * @since   1.0
+	 */
+	public function setData($data)
+	{
+		$this->data = $data;
+	}
+
+	/**
+	 * Unloads data from renderer
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function unsetData()
+	{
+		$this->data = array();
+	}
+
+	/**
+	 * Sets a piece of data
+	 *
+	 * @param   string  $key    Name of variable
+	 * @param   string  $value  Value of variable
+	 *
+	 * @return  RendererInterface  Returns self for chaining
+	 *
+	 * @since   1.0
+	 */
+	public function set($key, $value)
+	{
+		// TODO Make use of Joomla\Registry to provide paths
+		$this->data[$key] = $value;
+	}
 }
--- src/TwigRenderer.php
+++ src/TwigRenderer.php
@@ -15,130 +15,145 @@ namespace BabDev\Renderer;
  */
 class TwigRenderer extends \Twig_Environment implements RendererInterface
 {
-        
-    private $_loader;
-    
-    private $_config;
-    
-    private $_data = array();
+	/**
+	 * Rendering engine
+	 *
+	 * @var    \Twig_Loader_Filesystem
+	 * @since  1.0
+	 */
+	private $loader;
 
-    /**
-     * A constructor method
-     *
-     * @param  array  $config  Configurations
-     *
-     * @return void
-     *
-     * @since   1.0
-     */
-    public function __construct($config = array())
-    {
-        $this->_config = $config;
-        $this->_loader = new Twig_Loader_Filesystem();
-    }
-    
-    /**
-     * Render and return compiled data.
-     *
-     * @param   string  $template  The template file name
-     * @param   array   $data      The data to pass to the template
-     *
-     * @return  string  Compiled data
-     *
-     * @since   1.0
-     */
-    public function render($template, array $data = array())
-    {
-        $data = array_merge($this->_data, $data);
-        
-        //TODO Process template name
-        
-        parent::render($template, $data);
-    }
-    
-    /**
-     * Add a folder with alias to the renderer
-     *
-     * @param  string  $alias      The folder alias
-     * @param  string  $directory  The folder path
-     * 
-     * @return  boolean  TRUE if the folder is loaded
-     *
-     * @since 1.0
-     */
-    public function addFolder($alias, $directory)
-    {
-        $this->_loader->addPath($directory, $alias);
-    }
-    
-    /**
-     * Sets file extension for template loader
-     *
-     * @param  string  $extension  Template files extension
-     *
-     * @return  void
-     *
-     * @since 1.0
-     */
-    public function setFileExtension($extension)
-    {
-        $this->config['file_extension'] = $extension;
-    }
-    
-    /**
-     * Checks if folder, folder alias, template or template path exists
-     *
-     * @param  string  $path  Full path or part of a path
-     *
-     * @return  boolean  TRUE of the path exists
-     *
-     * @since  1.0
-     */
-    public function pathExists($path)
-    {
-        //@TODO check for directories
-        return $this->_loader->exists($path);
-    }
-    
-    /**
-     * Loads data from array into the renderer
-     *
-     * @param  array  $data  Array of variables
-     *
-     * @return boolean  TRUE if data loaded successfully
-     *
-     * @since  1.0
-     */
-    public function setData($data)
-    {
-        $this->_data = $data; 
-    }
-    
-    /**
-     * Unloads data from renderer
-     *
-     * @return void
-     *
-     * @since  1.0
-     */
-    public function unsetData()
-    {
-        $this->_data = array();
-    }
-    
-    /**
-     * Sets a piece of data
-     *
-     * @param  string  $key    Name of variable
-     * @param  string  $value  Value of variable
-     *
-     * @return RendererInterface  Returns self for chaining
-     *
-     * @since  1.0
-     */
-    public function set($key, $value)
-    {
-        //TODO Make use of Joomla\Registry to provide paths
-        $this->_data[$key] = $value;
-    }
+	/**
+	 * Configuration array
+	 *
+	 * @var    array
+	 * @since  1.0
+	 */
+	private $config;
+
+	/**
+	 * Data for output by the renderer
+	 *
+	 * @var    array
+	 * @since  1.0
+	 */
+	private $data = array();
+
+	/**
+	 * A constructor method
+	 *
+	 * @param   array  $config  Configurations
+	 *
+	 * @since   1.0
+	 */
+	public function __construct($config = array())
+	{
+		$this->config = $config;
+		$this->loader = new Twig_Loader_Filesystem;
+	}
+
+	/**
+	 * Render and return compiled data.
+	 *
+	 * @param   string  $template  The template file name
+	 * @param   array   $data      The data to pass to the template
+	 *
+	 * @return  string  Compiled data
+	 *
+	 * @since   1.0
+	 */
+	public function render($template, array $data = array())
+	{
+		$data = array_merge($this->data, $data);
+
+		// TODO Process template name
+
+		parent::render($template, $data);
+	}
+
+	/**
+	 * Add a folder with alias to the renderer
+	 *
+	 * @param   string  $alias      The folder alias
+	 * @param   string  $directory  The folder path
+	 *
+	 * @return  boolean  True if the folder is loaded
+	 *
+	 * @since   1.0
+	 */
+	public function addFolder($alias, $directory)
+	{
+		$this->loader->addPath($directory, $alias);
+	}
+
+	/**
+	 * Sets file extension for template loader
+	 *
+	 * @param   string  $extension  Template files extension
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function setFileExtension($extension)
+	{
+		$this->config['file_extension'] = $extension;
+	}
+
+	/**
+	 * Checks if folder, folder alias, template or template path exists
+	 *
+	 * @param   string  $path  Full path or part of a path
+	 *
+	 * @return  boolean  TRUE of the path exists
+	 *
+	 * @since   1.0
+	 */
+	public function pathExists($path)
+	{
+		// TODO check for directories
+		return $this->loader->exists($path);
+	}
+
+	/**
+	 * Loads data from array into the renderer
+	 *
+	 * @param   array  $data  Array of variables
+	 *
+	 * @return  boolean  True if data loaded successfully
+	 *
+	 * @since   1.0
+	 */
+	public function setData($data)
+	{
+		$this->data = $data;
+	}
+
+	/**
+	 * Unloads data from renderer
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function unsetData()
+	{
+		$this->data = array();
+	}
+
+	/**
+	 * Sets a piece of data
+	 *
+	 * @param   string  $key    Name of variable
+	 * @param   string  $value  Value of variable
+	 *
+	 * @return  RendererInterface  Returns self for chaining
+	 *
+	 * @since   1.0
+	 */
+	public function set($key, $value)
+	{
+		// TODO Make use of Joomla\Registry to provide paths
+		$this->data[$key] = $value;
+	}
 }
