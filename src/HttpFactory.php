df9e4bab5a25f8e6df5a9f3ed02354aa0d9b5b0c
George Wilson <georgejameswilson@googlemail.com>
2014-09-22 08:15:42 +0100

    Prioritise Curl (see https://github.com/joomla/joomla-cms/pull/4105)

--- src/HttpFactory.php
+++ src/HttpFactory.php
@@ -109,6 +109,16 @@ class HttpFactory
 		// Keep alphabetical order across all environments
 		sort($names);
 
+
+		// If curl is available set it to the first position
+		$key = array_search('curl', $names);
+
+		if ($key)
+		{
+			unset($names[$key]);
+			array_unshift($names, 'curl');
+		}
+
 		return $names;
 	}
 }
