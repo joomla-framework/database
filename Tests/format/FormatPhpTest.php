cea08d23ccda6aa65d314f20b6a00bdc24068f69
Michael Babker <mbabker@flbab.com>
2014-10-08 12:04:30 -0400

    Add namespace support for PHP format objects (Fix #17)

--- Tests/format/FormatPhpTest.php
+++ Tests/format/FormatPhpTest.php
@@ -57,6 +57,48 @@ class JRegistryFormatPHPTest extends \PHPUnit_Framework_TestCase
 	}
 
 	/**
+	 * Test the Php::objectToString method with a PHP namespace in the options.
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function testObjectToStringNamespace()
+	{
+		$class = AbstractRegistryFormat::getInstance('PHP');
+		$options = array('class' => 'myClass', 'namespace' => 'Joomla\\Registry\\Test');
+		$object = new \stdClass;
+		$object->foo = 'bar';
+		$object->quoted = '"stringwithquotes"';
+		$object->booleantrue = true;
+		$object->booleanfalse = false;
+		$object->numericint = 42;
+		$object->numericfloat = 3.1415;
+
+		// The PHP registry format does not support nested objects
+		$object->section = new \stdClass;
+		$object->section->key = 'value';
+		$object->array = array('nestedarray' => array('test1' => 'value1'));
+
+		$string = "<?php\n" .
+			"namespace Joomla\\Registry\\Test;\n\n" .
+			"class myClass {\n" .
+			"\tpublic \$foo = 'bar';\n" .
+			"\tpublic \$quoted = '\"stringwithquotes\"';\n" .
+			"\tpublic \$booleantrue = '1';\n" .
+			"\tpublic \$booleanfalse = '';\n" .
+			"\tpublic \$numericint = '42';\n" .
+			"\tpublic \$numericfloat = '3.1415';\n" .
+			"\tpublic \$section = array(\"key\" => \"value\");\n" .
+			"\tpublic \$array = array(\"nestedarray\" => array(\"test1\" => \"value1\"));\n" .
+			"}\n?>";
+		$this->assertThat(
+			$class->objectToString($object, $options),
+			$this->equalTo($string)
+		);
+	}
+
+	/**
 	 * Test the Php::stringToObject method.
 	 *
 	 * @return  void
