7d009b687e53030bf27f59ed3314a801316a3d4f
George Wilson <georgejameswilson@googlemail.com>
2015-07-14 18:36:08 +0100

    Get server data through JInput

--- src/AbstractWebApplication.php
+++ src/AbstractWebApplication.php
@@ -589,21 +589,21 @@ abstract class AbstractWebApplication extends AbstractApplication
 		 */
 
 		// If PHP_SELF and REQUEST_URI are both populated then we will assume "Apache Mode".
-		if (!empty($_SERVER['PHP_SELF']) && !empty($_SERVER['REQUEST_URI']))
+		if (!empty($this->input->server->getRaw('PHP_SELF'])) && !empty($this->input->server->getRaw('REQUEST_URI')))
 		{
 			// The URI is built from the HTTP_HOST and REQUEST_URI environment variables in an Apache environment.
-			$uri = $scheme . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+			$uri = $scheme . $this->input->server->getRaw('HTTP_HOST') . $this->input->server->getRaw('REQUEST_URI');
 		}
 		else
 		// If not in "Apache Mode" we will assume that we are in an IIS environment and proceed.
 		{
 			// IIS uses the SCRIPT_NAME variable instead of a REQUEST_URI variable... thanks, MS
-			$uri = $scheme . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
+			$uri = $scheme . $this->input->server->getRaw('HTTP_HOST') . $this->input->server->getRaw('SCRIPT_NAME');
 
 			// If the QUERY_STRING variable exists append it to the URI string.
-			if (isset($_SERVER['QUERY_STRING']) && !empty($_SERVER['QUERY_STRING']))
+			if (!empty($this->input->server->getRaw('QUERY_STRING', '')))
 			{
-				$uri .= '?' . $_SERVER['QUERY_STRING'];
+				$uri .= '?' . $this->input->server->getRaw('QUERY_STRING', '');
 			}
 		}
 
@@ -640,7 +640,7 @@ abstract class AbstractWebApplication extends AbstractApplication
 	 */
 	public function isSSLConnection()
 	{
-		return (!empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) != 'off');
+		return (!empty($this->input->server->getRaw('HTTPS', '')) && strtolower($this->input->server->getRaw('HTTPS', '')) != 'off');
 	}
 
 	/**
