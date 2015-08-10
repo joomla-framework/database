c3b0a59fbccd79fe2a74855df784e07a3d755c76
javier gomez <javier.gomez.corio@gmail.com>
2015-08-10 11:41:33 +0200

    Document the possibility of getting a Release by tag
    Document that you can get a release by tag witch a special paramenter. See: https://developer.github.com/v3/repos/releases/#get-a-release-by-tag-name

--- src/Package/Repositories/Releases.php
+++ src/Package/Repositories/Releases.php
@@ -127,7 +127,7 @@ class Releases extends AbstractPackage
 	 *
 	 * @param   string  $user  The name of the owner of the GitHub repository.
 	 * @param   string  $repo  The name of the GitHub repository.
-	 * @param   string  $ref   The reference to get.
+	 * @param   string  $ref   Valid values are: 'latest', to get latest release. 'tag/2.0.24' for geting Release by tag. Or Release Id, for example: '1643513'
 	 *
 	 * @return  object
 	 *
