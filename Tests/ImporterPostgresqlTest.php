28c67561cac4b5c465cf8881922d981ca316613d
Michael Babker <mbabker@flbab.com>
2015-09-05 12:35:10 -0400

    Remove test cases that tested PHP language features

--- Tests/ImporterPostgresqlTest.php
+++ Tests/ImporterPostgresqlTest.php
@@ -994,32 +994,6 @@ class ImporterPostgresqlTest extends \PHPUnit_Framework_TestCase
 	 *
 	 * @since  1.0
 	 */
-	public function testSetDboWithBadInput()
-	{
-		$instance	= new ImporterPostgresqlInspector;
-
-		try
-		{
-			$instance->setDbo(new \stdClass);
-		}
-		catch (\PHPUnit_Framework_Error $e)
-		{
-			// Expecting the error, so just ignore it.
-			return;
-		}
-
-		$this->fail(
-			'setDbo requires a JDatabaseDriverPostgresql object and should throw an exception.'
-		);
-	}
-
-	/**
-	 * Tests the setDbo method with the wrong type of class.
-	 *
-	 * @return void
-	 *
-	 * @since  1.0
-	 */
 	public function testSetDboWithGoodInput()
 	{
 		$instance = new ImporterPostgresqlInspector;
