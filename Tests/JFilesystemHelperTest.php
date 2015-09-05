a0c5a557519b9e6c131db5065ffcda930aecfbe8
Michael Babker <mbabker@flbab.com>
2015-09-05 12:20:02 -0400

    Fix element order in tests...

--- Tests/JFilesystemHelperTest.php
+++ Tests/JFilesystemHelperTest.php
@@ -133,7 +133,7 @@ class FilesystemHelperTest extends PHPUnit_Framework_TestCase
 		$streams = Helper::getJStreams();
 
 		$this->assertEquals(
-			array('String', 'StringWrapper'),
+			array('StringWrapper', 'String'),
 			$streams
 		);
 	}
