3af9549eb71aa93db43bfce0cde47bfb2c751ad5
Michael Babker <mbabker@flbab.com>
2015-09-18 22:44:42 -0400

    Add two custom Exception types

---
+++ src/Exception/UnexpectedResponseException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Part of the Joomla Framework Http Package
+ *
+ * @copyright  Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Joomla\Http\Exception;
+
+/**
+ * Exception representing an unexpected response
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class UnexpectedResponseException extends \DomainException
+{
+}
