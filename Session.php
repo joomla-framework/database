2bd5de273be27920e99e8c7dadd974c010be297f
Michael Babker <mbabker@flbab.com>
2015-12-14 13:22:25 -0500

    Fix Joomla security notice [20151205]
    Please see https://developer.joomla.org/security-centre/637-20151205-session-remote-code-execution-vulnerability.html for additional details

--- Session.php
+++ Session.php
@@ -112,7 +112,6 @@ class Session implements \IteratorAggregate
 	 *
 	 * @var    Input
 	 * @since  1.0
-	 * @deprecated  2.0
 	 */
 	private $input = null;
 
@@ -1036,7 +1035,6 @@ class Session implements \IteratorAggregate
 
 			$this->set('session.client.address', null);
 			$this->set('session.client.forwarded', null);
-			$this->set('session.client.browser', null);
 			$this->set('session.token', null);
 		}
 
@@ -1055,22 +1053,18 @@ class Session implements \IteratorAggregate
 			}
 		}
 
-		// Record proxy forwarded for in the session in case we need it later
-		if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-		{
-			$this->set('session.client.forwarded', $_SERVER['HTTP_X_FORWARDED_FOR']);
-		}
+		$remoteAddr = $this->input->server->getString('REMOTE_ADDR', '');
 
 		// Check for client address
-		if (in_array('fix_adress', $this->security) && isset($_SERVER['REMOTE_ADDR']))
+		if (in_array('fix_adress', $this->security) && !empty($remoteAddr) && filter_var($remoteAddr, FILTER_VALIDATE_IP) !== false)
 		{
 			$ip = $this->get('session.client.address');
 
 			if ($ip === null)
 			{
-				$this->set('session.client.address', $_SERVER['REMOTE_ADDR']);
+				$this->set('session.client.address', $remoteAddr);
 			}
-			elseif ($_SERVER['REMOTE_ADDR'] !== $ip)
+			elseif ($remoteAddr !== $ip)
 			{
 				$this->setState('error');
 
@@ -1078,20 +1072,12 @@ class Session implements \IteratorAggregate
 			}
 		}
 
-		// Check for clients browser
-		if (in_array('fix_browser', $this->security) && isset($_SERVER['HTTP_USER_AGENT']))
-		{
-			$browser = $this->get('session.client.browser');
+		$xForwardedFor = $this->input->server->getString('HTTP_X_FORWARDED_FOR', '');
 
-			if ($browser === null)
-			{
-				$this->set('session.client.browser', $_SERVER['HTTP_USER_AGENT']);
-			}
-			elseif ($_SERVER['HTTP_USER_AGENT'] !== $browser)
-			{
-				// @todo remove code: 				$this->_state	=	'error';
-				// @todo remove code: 				return false;
-			}
+		// Record proxy forwarded for in the session in case we need it later
+		if (!empty($xForwardedFor) && filter_var($xForwardedFor, FILTER_VALIDATE_IP) !== false)
+		{
+			$this->set('session.client.forwarded', $xForwardedFor);
 		}
 
 		return true;
