97c0050aa11ac72ef3a53b66d70accbf92fe07b4
Michael Babker <mbabker@flbab.com>
2016-01-02 14:14:36 -0500

    Set a default class name for the PHP format

--- Tests/format/FormatPhpTest.php
+++ Tests/format/FormatPhpTest.php
@@ -57,6 +57,46 @@ class JRegistryFormatPHPTest extends \PHPUnit_Framework_TestCase
 	}
 
 	/**
+	 * Test the Php::objectToString method.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.5.1
+	 */
+	public function testObjectToStringNoClass()
+	{
+		$class = AbstractRegistryFormat::getInstance('PHP');
+		$object = new \stdClass;
+		$object->foo = 'bar';
+		$object->quoted = '"stringwithquotes"';
+		$object->booleantrue = true;
+		$object->booleanfalse = false;
+		$object->numericint = 42;
+		$object->numericfloat = 3.1415;
+
+		// The PHP registry format does not support nested objects
+		$object->section = new \stdClass;
+		$object->section->key = 'value';
+		$object->array = array('nestedarray' => array('test1' => 'value1'));
+
+		$string = "<?php\n" .
+			"class Registry {\n" .
+			"\tpublic \$foo = 'bar';\n" .
+			"\tpublic \$quoted = '\"stringwithquotes\"';\n" .
+			"\tpublic \$booleantrue = '1';\n" .
+			"\tpublic \$booleanfalse = '';\n" .
+			"\tpublic \$numericint = '42';\n" .
+			"\tpublic \$numericfloat = '3.1415';\n" .
+			"\tpublic \$section = array(\"key\" => \"value\");\n" .
+			"\tpublic \$array = array(\"nestedarray\" => array(\"test1\" => \"value1\"));\n" .
+			"}\n?>";
+		$this->assertThat(
+			$class->objectToString($object),
+			$this->equalTo($string)
+		);
+	}
+
+	/**
 	 * Test the Php::objectToString method with a PHP namespace in the options.
 	 *
 	 * @return  void
