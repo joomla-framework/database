2a0e9a0387d06758a933e9b2cd24c76f0a039633
Michael Babker <mbabker@flbab.com>
2017-01-22 12:26:06 -0600

    Test the traits

---
+++ Tests/StatefulModelTraitTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @copyright  Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Joomla\Model\Tests;
+
+/**
+ * Tests for \Joomla\Model\StatefulModelTrait.
+ */
+class StatefulModelTraitTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @testdox  Calling getState() without a state set will throw an Exception
+	 *
+	 * @requires            PHP 5.4
+	 * @covers              Joomla\Model\StatefulModelTraitTest::getState
+	 * @expectedException   \UnexpectedValueException
+	 */
+	public function testgetStateException()
+	{
+		/** @var \Joomla\Model\StatefulModelTrait $object */
+		$object = $this->getObjectForTrait('\\Joomla\\Model\\StatefulModelTrait');
+		$object->getState();
+	}
+
+	/**
+	 * @testdox  A Registry representing the state is set and retrieved
+	 *
+	 * @requires  PHP 5.4
+	 * @covers    Joomla\Model\StatefulModelTraitTest::getState
+	 * @covers    Joomla\Model\StatefulModelTraitTest::setState
+	 */
+	public function testSetAndgetState()
+	{
+		/** @var \Joomla\Model\StatefulModelTrait $object */
+		$object = $this->getObjectForTrait('\\Joomla\\Model\\StatefulModelTrait');
+
+		/** @var \Joomla\Registry\Registry $state */
+		$state = $this->getMockBuilder('\\Joomla\\Registry\\Registry')
+			->getMock();
+
+		$object->setState($state);
+
+		$this->assertSame($state, $object->getState());
+	}
+}
