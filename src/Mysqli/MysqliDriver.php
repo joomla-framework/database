6377af6b8f1940497b02a7c0075cbfc2bb9f31e5
Nicola Galgano <optimus4joomla@gmail.com>
2017-06-20 18:26:50 +0200

    missed getConnectionCollation
    Pull Request for Issue #16789 .
    
    ### Summary of Changes
    added the missed `getConnectionCollation()` method
    
    
    ### Testing Instructions
    Access to System -> System Information
    
    
    ### Expected result
    NO error
    
    
    ### Actual result
    Call to undefined method Joomla\Database\Mysqli\MysqliDriver::getConnectionCollation()

--- src/Mysqli/MysqliDriver.php
+++ src/Mysqli/MysqliDriver.php
@@ -389,6 +389,22 @@ class MysqliDriver extends DatabaseDriver
 	}
 
 	/**
+	 * Method to get the database connection collation in use by sampling a text field of a table in the database.
+	 *
+	 * @return  mixed  The collation in use by the database connection (string) or boolean false if not supported.
+	 *
+	 * @since   1.0
+	 * @throws  \RuntimeException
+	 */
+	public function getConnectionCollation()
+	{
+		$this->connect();
+
+		return $this->setQuery('SELECT @@collation_connection;')->loadResult();
+
+	}
+	
+	/**
 	 * Get the number of returned rows for the previous executed SQL statement.
 	 *
 	 * @param   resource  $cursor  An optional database cursor resource to extract the row count from.
