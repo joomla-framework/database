33296c398f309feee352e35bd77941aa54677b43
Michael Babker <michael.babker@gmail.com>
2017-08-20 14:47:53 -0500

    Stub application

---
+++ src/Application.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Part of the Joomla Framework Console Package
+ *
+ * @copyright  Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Joomla\Console;
+
+use Joomla\Application\AbstractApplication;
+use Joomla\Input\Cli;
+use Joomla\Registry\Registry;
+
+/**
+ * Base application class for a Joomla! command line application.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class Application extends AbstractApplication
+{
+	/**
+	 * Class constructor.
+	 *
+	 * @param   Cli       $input   An optional argument to provide dependency injection for the application's input object.  If the argument is an
+	 *                             Input object that object will become the application's input object, otherwise a default input object is created.
+	 * @param   Registry  $config  An optional argument to provide dependency injection for the application's config object.  If the argument
+	 *                             is a Registry object that object will become the application's config object, otherwise a default config
+	 *                             object is created.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function __construct(Cli $input = null, Registry $config = null)
+	{
+		// Close the application if we are not executed from the command line.
+		if (!defined('STDOUT') || !defined('STDIN') || !isset($_SERVER['argv']))
+		{
+			$this->close();
+		}
+
+		// Call the constructor as late as possible (it runs `initialise`).
+		parent::__construct($input ?: new Cli, $config);
+
+		// Set the current directory.
+		$this->set('cwd', getcwd());
+	}
+
+	/**
+	 * Method to run the application routines.
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected function doExecute()
+	{
+
+	}
+}
