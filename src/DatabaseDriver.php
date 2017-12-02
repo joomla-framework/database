ba5ce06733722bd2e6b3d9bc01d915b80021e5a5
Michael Babker <michael.babker@gmail.com>
2017-12-02 14:34:33 -0600

    Use trigger_error for deprecations

--- src/DatabaseDriver.php
+++ src/DatabaseDriver.php
@@ -427,19 +427,29 @@ abstract class DatabaseDriver implements DatabaseInterface, DispatcherAwareInter
 	 * @return  mixed   A value if the property name is valid, null otherwise.
 	 *
 	 * @since       1.4.0
-	 * @deprecated  1.4.0  This is a B/C proxy since $this->name was previously public
+	 * @deprecated  3.0  This is a B/C proxy since $this->name was previously public
 	 */
 	public function __get($name)
 	{
 		switch ($name)
 		{
 			case 'name':
+				@trigger_error(
+					'Accessing the name property of the database driver is deprecated, use the getName() method instead.',
+					E_USER_DEPRECATED
+				);
+
 				return $this->getName();
 
 			default:
 				$trace = debug_backtrace();
 				trigger_error(
-					'Undefined property via __get(): ' . $name . ' in ' . $trace[0]['file'] . ' on line ' . $trace[0]['line'],
+					sprintf(
+						'Undefined property via __get(): %1$s in %2$s on line %3$s',
+						$name,
+						$trace[0]['file'],
+						$trace[0]['line']
+					),
 					E_USER_NOTICE
 				);
 		}
