468d564f492be1625a9b564587b2a6536b111713
Michael Babker <michael.babker@gmail.com>
2018-03-12 20:48:53 -0500

    Prep release

--- src/Transport/Curl.php
+++ src/Transport/Curl.php
@@ -2,7 +2,7 @@
 /**
  * Part of the Joomla Framework Http Package
  *
- * @copyright  Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @copyright  Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
@@ -323,7 +323,7 @@ class Curl implements TransportInterface
 	 *
 	 * @return  integer
 	 *
-	 * @since   __DEPLOY_VERSION__
+	 * @since   1.3.1
 	 */
 	private function mapProtocolVersion($version)
 	{
