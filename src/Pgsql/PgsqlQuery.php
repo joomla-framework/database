f90bb871a378d2d3758f26dc195a33c8d5ee7844
Michael Babker <michael.babker@gmail.com>
2018-03-18 11:11:48 -0500

    Have PDO PostgreSQL query extend PDO query, make a trait for PostgreSQL query methods

--- src/Pgsql/PgsqlQuery.php
+++ src/Pgsql/PgsqlQuery.php
@@ -8,96 +8,17 @@
 
 namespace Joomla\Database\Pgsql;
 
-use Joomla\Database\Postgresql\PostgresqlQuery;
-use Joomla\Database\Query\PreparableInterface;
+use Joomla\Database\Pdo\PdoQuery;
+use Joomla\Database\Query\PostgresqlQueryBuilder;
 
 /**
  * PDO PostgreSQL Query Building Class.
  *
  * @since  1.0
  */
-class PgsqlQuery extends PostgresqlQuery implements PreparableInterface
+class PgsqlQuery extends PdoQuery
 {
-	/**
-	 * Holds key / value pair of bound objects.
-	 *
-	 * @var    mixed
-	 * @since  1.5.0
-	 */
-	protected $bounded = [];
-
-	/**
-	 * Method to add a variable to an internal array that will be bound to a prepared SQL statement before query execution. Also
-	 * removes a variable that has been bounded from the internal bounded array when the passed in value is null.
-	 *
-	 * @param   string|integer  $key            The key that will be used in your SQL query to reference the value. Usually of
-	 *                                          the form ':key', but can also be an integer.
-	 * @param   mixed           $value          The value that will be bound. The value is passed by reference to support output
-	 *                                          parameters such as those possible with stored procedures.
-	 * @param   integer         $dataType       Constant corresponding to a SQL datatype.
-	 * @param   integer         $length         The length of the variable. Usually required for OUTPUT parameters.
-	 * @param   array           $driverOptions  Optional driver options to be used.
-	 *
-	 * @return  $this
-	 *
-	 * @since   1.5.0
-	 */
-	public function bind($key = null, &$value = null, $dataType = \PDO::PARAM_STR, $length = 0, $driverOptions = array())
-	{
-		// Case 1: Empty Key (reset $bounded array)
-		if (empty($key))
-		{
-			$this->bounded = array();
-
-			return $this;
-		}
-
-		// Case 2: Key Provided, null value (unset key from $bounded array)
-		if (is_null($value))
-		{
-			if (isset($this->bounded[$key]))
-			{
-				unset($this->bounded[$key]);
-			}
-
-			return $this;
-		}
-
-		$obj = new \stdClass;
-
-		$obj->value = &$value;
-		$obj->dataType = $dataType;
-		$obj->length = $length;
-		$obj->driverOptions = $driverOptions;
-
-		// Case 3: Simply add the Key/Value into the bounded array
-		$this->bounded[$key] = $obj;
-
-		return $this;
-	}
-
-	/**
-	 * Retrieves the bound parameters array when key is null and returns it by reference. If a key is provided then that item is
-	 * returned.
-	 *
-	 * @param   mixed  $key  The bounded variable key to retrieve.
-	 *
-	 * @return  mixed
-	 *
-	 * @since   1.5.0
-	 */
-	public function &getBounded($key = null)
-	{
-		if (empty($key))
-		{
-			return $this->bounded;
-		}
-
-		if (isset($this->bounded[$key]))
-		{
-			return $this->bounded[$key];
-		}
-	}
+	use PostgresqlQueryBuilder;
 
 	/**
 	 * Clear data from the query or a specific clause of the query.
@@ -106,17 +27,62 @@ class PgsqlQuery extends PostgresqlQuery implements PreparableInterface
 	 *
 	 * @return  $this
 	 *
-	 * @since   1.5.0
+	 * @since   1.0
 	 */
 	public function clear($clause = null)
 	{
 		switch ($clause)
 		{
-			case null:
-				$this->bounded = array();
+			case 'limit':
+				$this->limit = null;
+				break;
+
+			case 'offset':
+				$this->offset = null;
+				break;
+
+			case 'forUpdate':
+				$this->forUpdate = null;
+				break;
+
+			case 'forShare':
+				$this->forShare = null;
+				break;
+
+			case 'noWait':
+				$this->noWait = null;
+				break;
+
+			case 'returning':
+				$this->returning = null;
+				break;
+
+			case 'select':
+			case 'update':
+			case 'delete':
+			case 'insert':
+			case 'from':
+			case 'join':
+			case 'set':
+			case 'where':
+			case 'group':
+			case 'having':
+			case 'order':
+			case 'columns':
+			case 'values':
+				parent::clear($clause);
+				break;
+
+			default:
+				$this->forUpdate = null;
+				$this->forShare = null;
+				$this->noWait = null;
+				$this->returning = null;
+
+				parent::clear($clause);
 				break;
 		}
 
-		return parent::clear($clause);
+		return $this;
 	}
 }
