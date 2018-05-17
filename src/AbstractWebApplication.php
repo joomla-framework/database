b85e79ec8a6fbe113ea7b9c4d32e770a715d925c
Michael Babker <michael.babker@gmail.com>
2018-05-17 07:27:30 -0500

    Improve readability

--- src/AbstractWebApplication.php
+++ src/AbstractWebApplication.php
@@ -788,10 +788,15 @@ abstract class AbstractWebApplication extends AbstractApplication
 	public function isSslConnection()
 	{
 		$serverSSLVar = $this->input->server->getString('HTTPS', '');
+
+		if (!empty($serverSSLVar) && strtolower($serverSSLVar) !== 'off')
+		{
+			return true;
+		}
+
 		$serverForwarderProtoVar = $this->input->server->getString('HTTP_X_FORWARDED_PROTO', '');
 
-		return (!empty($serverSSLVar) && strtolower($serverSSLVar) != 'off') ||
-		(!empty($serverForwarderProtoVar) && strtolower($serverForwarderProtoVar) == 'https');;
+		return !empty($serverForwarderProtoVar) && strtolower($serverForwarderProtoVar) === 'https';
 	}
 
 	/**
