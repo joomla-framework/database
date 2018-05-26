ff97c08aa5fa0f48ccc3e7444827f7f9a6b904c8
Michael Babker <michael.babker@gmail.com>
2018-05-26 12:08:08 -0500

    Remove typehinting of source argument

--- src/Json.php
+++ src/Json.php
@@ -34,7 +34,7 @@ class Json extends Input
 	 *
 	 * @since   1.0
 	 */
-	public function __construct(array $source = null, array $options = array())
+	public function __construct($source = null, array $options = array())
 	{
 		if (isset($options['filter']))
 		{
