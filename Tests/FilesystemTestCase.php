f004cc3a2c95e6b09e85fe692f924eccbff8c69a
Michael Babker <michael.babker@gmail.com>
2018-06-03 00:41:25 -0500

    We should interact with the real filesystem for tests since we're testing the filesystem API, refactor Folder tests to do so

---
+++ Tests/FilesystemTestCase.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @copyright  Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Joomla\Filesystem\Tests;
+
+use Joomla\Filesystem\Folder;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Base test case for filesystem interacting tests
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class FilesystemTestCase extends TestCase
+{
+	/**
+	 * Path to the test space
+	 *
+	 * @var    null|string
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $testPath = null;
+
+	/**
+	 * Storage for the system's umask
+	 *
+	 * @var    integer
+	 * @since  __DEPLOY_VERSION__
+	 */
+	private $umask;
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected function setUp()
+	{
+		$this->umask    = umask(0);
+		$this->testPath = sys_get_temp_dir() . '/' . microtime(true) . '.' . mt_rand();
+
+		mkdir($this->testPath, 0777, true);
+
+		$this->testPath = realpath($this->testPath);
+	}
+
+	/**
+	 * Tears down the fixture, for example, close a network connection.
+	 * This method is called after a test is executed.
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected function tearDown()
+	{
+		Folder::delete($this->testPath);
+
+		umask($this->umask);
+	}
+}
