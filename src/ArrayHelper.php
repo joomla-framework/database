afd6b1db3eb43909aa07b276f163c73e166c6c71
Michael Babker <michael.babker@gmail.com>
2018-10-17 18:05:08 -0500

    Note B/C break that will occur in 2.0 regarding the flatten method

--- src/ArrayHelper.php
+++ src/ArrayHelper.php
@@ -722,6 +722,7 @@ final class ArrayHelper
 	 * @return  array
 	 *
 	 * @since   1.3.0
+	 * @note    As of 2.0, the result will not include the original array structure
 	 */
 	public static function flatten($array, $separator = '.', $prefix = '')
 	{
