96c52927cad82ec8be0002f3b434588de69bd29b
Michael Babker <michael.babker@gmail.com>
2019-02-17 15:57:21 -0600

    Move test case with other write method tests, don't need to unlink in each test case as tearDown covers it

--- Tests/FileTest.php
+++ Tests/FileTest.php
@@ -402,6 +402,37 @@ class FileTest extends FilesystemTestCase
 	}
 
 	/**
+	 * Test write method when appending to a file.
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 *
+	 */
+	public function testWriteWithAppend()
+	{
+		$name = 'tempFile.txt';
+		$data = 'Lorem ipsum dolor sit amet';
+		$appendData = PHP_EOL . $data;
+
+		$this->assertTrue(
+			File::write($this->testPath . '/' . $name, $data),
+			'The file was not written.'
+		);
+
+		$this->assertTrue(
+			File::write($this->testPath . '/' . $name, $appendData, false, true),
+			'The file was not appended.'
+		);
+
+		$this->assertStringEqualsFile(
+			$this->testPath . '/' . $name,
+			$data . $appendData,
+			'The written file should match the given content.'
+		);
+	}
+
+	/**
 	 * Test write method.
 	 *
 	 * @return  void
@@ -549,38 +580,5 @@ class FileTest extends FilesystemTestCase
 		$this->assertTrue(
 			File::upload($this->testPath . '/' . $name . '.txt', $this->testPath . '/' . $name . '/' . $uploadedFileName)
 		);
-	}
-
-	/**
-	 * Test the new append mode in the write method.
-	 *
-	 * @return  void
-	 *
-	 * @since   __DEPLOY_VERSION__
-	 *
-	 */
-	public function testWriteWithAppend()
-	{
-		$name = 'tempFile.txt';
-		$data = 'Lorem ipsum dolor sit amet';
-		$appendData = PHP_EOL . $data;
-
-		$this->assertTrue(
-			File::write($this->testPath . '/' . $name, $data),
-			'The file was not written.'
-		);
-
-		$this->assertTrue(
-			File::write($this->testPath . '/' . $name, $appendData, false, true),
-			'The file was not appended.'
-		);
-
-		$this->assertStringEqualsFile(
-			$this->testPath . '/' . $name,
-			$data . $appendData,
-			'The written file should match the given content.'
-		);
-
-		unlink($this->testPath . '/' . $name);
 	}
 }
