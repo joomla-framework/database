ac557447e3193d69967d10e720eab9b582d841d7
Joomla Jenkins <release+joomla-jenkins@joomla.org>
2019-03-10 10:20:33 -0500

    Tagging release 1.4.0

--- Storage/Apcu.php
+++ Storage/Apcu.php
@@ -2,7 +2,7 @@
 /**
  * Part of the Joomla Framework Session Package
  *
- * @copyright  Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @copyright  Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
@@ -14,7 +14,7 @@ use Joomla\Session\Storage;
  * APCU session storage handler for PHP
  *
  * @link        https://secure.php.net/manual/en/function.session-set-save-handler.php
- * @since       __DEPLOY_VERSION__
+ * @since       1.4.0
  * @deprecated  2.0  The Storage class chain will be removed.
  */
 class Apcu extends Storage
@@ -24,7 +24,7 @@ class Apcu extends Storage
 	 *
 	 * @param   array  $options  Optional parameters
 	 *
-	 * @since   __DEPLOY_VERSION__
+	 * @since   1.4.0
 	 * @throws  \RuntimeException
 	 */
 	public function __construct($options = array())
@@ -45,7 +45,7 @@ class Apcu extends Storage
 	 *
 	 * @return  string  The session data.
 	 *
-	 * @since   __DEPLOY_VERSION__
+	 * @since   1.4.0
 	 */
 	public function read($id)
 	{
@@ -62,7 +62,7 @@ class Apcu extends Storage
 	 *
 	 * @return  boolean  True on success, false otherwise.
 	 *
-	 * @since   __DEPLOY_VERSION__
+	 * @since   1.4.0
 	 */
 	public function write($id, $session_data)
 	{
@@ -78,7 +78,7 @@ class Apcu extends Storage
 	 *
 	 * @return  boolean  True on success, false otherwise.
 	 *
-	 * @since   __DEPLOY_VERSION__
+	 * @since   1.4.0
 	 */
 	public function destroy($id)
 	{
@@ -92,7 +92,7 @@ class Apcu extends Storage
 	 *
 	 * @return  boolean  True on success, false otherwise.
 	 *
-	 * @since   __DEPLOY_VERSION__
+	 * @since   1.4.0
 	 */
 	public static function isSupported()
 	{
