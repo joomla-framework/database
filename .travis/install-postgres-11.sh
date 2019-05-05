c79767e31bcd40e8fc25bda7d744b30a624fd09e
Michael Babker <michael.babker@gmail.com>
2019-05-05 13:00:25 -0500

    Tweak PG11 setup

--- .travis/install-postgres-11.sh
+++ .travis/install-postgres-11.sh
@@ -5,5 +5,6 @@ set -ex
 echo "Installing Postgres 11"
 sudo service postgresql stop || true
 
-sudo docker run -d --name postgres11 -p 5432:5432 postgres:11.1
+sudo docker pull postgres:11
+sudo docker run -d --name postgres11 -p 5432:5432 postgres:11
 sudo docker exec -i postgres11 bash <<< 'until pg_isready -U postgres > /dev/null 2>&1 ; do sleep 1; done'
