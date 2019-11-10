94b539151990c78afd94f737f6cf8b2c9c55d4c7
Michael Babker <michael.babker@gmail.com>
2019-11-10 14:17:50 -0600

    SQL Server statement test case updated

--- Tests/Sqlsrv/SqlsrvStatementTest.php
+++ Tests/Sqlsrv/SqlsrvStatementTest.php
@@ -6,74 +6,108 @@
 
 namespace Joomla\Database\Tests\Sqlqrv;
 
+use Joomla\Database\DatabaseDriver;
+use Joomla\Database\Exception\ExecutionFailureException;
 use Joomla\Database\Sqlsrv\SqlsrvStatement;
-use Joomla\Database\Tests\Cases\SqlsrvCase;
+use Joomla\Test\DatabaseTestCase;
 
 /**
- * Test class for \Joomla\Database\Sqlsrv\SqlsrvStatement.
- *
- * @since  1.0
+ * Test class for Joomla\Database\Sqlsrv\SqlsrvStatement
  */
-class SqlsrvStatementTest extends SqlsrvCase
+class SqlsrvStatementTest extends DatabaseTestCase
 {
 	/**
-	 * Regression test to ensure that named values with matching named params are correctly prepared.
-     * This simulates a whereIn condition
+	 * This method is called before the first test of this test class is run.
 	 *
 	 * @return  void
+	 */
+	public static function setUpBeforeClass(): void
+	{
+		parent::setUpBeforeClass();
+
+		if (!static::$connection || static::$connection->getName() !== 'sqlsrv')
+		{
+			self::markTestSkipped('SQL Server database not configured.');
+		}
+	}
+
+	/**
+	 * Sets up the fixture.
 	 *
-	 * @since   __DEPLOY_VERSION
+	 * This method is called before a test is executed.
+	 *
+	 * @return  void
+	 */
+	protected function setUp(): void
+	{
+		parent::setUp();
+
+		try
+		{
+			foreach (DatabaseDriver::splitSql(file_get_contents(dirname(__DIR__) . '/Stubs/Schema/sqlsrv.sql')) as $query)
+			{
+				static::$connection->setQuery($query)
+					->execute();
+			}
+		}
+		catch (ExecutionFailureException $exception)
+		{
+			$this->markTestSkipped(
+				\sprintf(
+					'Could not load SQL Server database: %s',
+					$exception->getMessage()
+				)
+			);
+		}
+	}
+
+	/**
+	 * Tears down the fixture.
+	 *
+	 * This method is called after a test is executed.
+	 */
+	protected function tearDown(): void
+	{
+		foreach (static::$connection->getTableList() as $table)
+		{
+			static::$connection->dropTable($table);
+		}
+	}
+
+	/**
+	 * Regression test to ensure that named values with matching named params are correctly prepared, this simulates a whereIn condition.
+	 *
+	 * @doesNotPerformAssertions
 	 */
 	public function testStatementPreparesManyArrayValues()
 	{
-		static::$driver->connect();
-		$query = 'SELECT * FROM dbtest WHERE id IN (:preparedArray1,:preparedArray2,:preparedArray3,:preparedArray4,:preparedArray5,:preparedArray6,'
-			. ':preparedArray7,:preparedArray8,:preparedArray9,:preparedArray10)';
+		$query = 'SELECT * FROM dbtest WHERE id IN (:preparedArray1,:preparedArray2,:preparedArray3,:preparedArray4,:preparedArray5,:preparedArray6,:preparedArray7,:preparedArray8,:preparedArray9,:preparedArray10)';
 
-        // Dummy assertion to ensure we haven't thrown an exception preparing the statement
-        $this->assertInstanceOf(
-            '\\Joomla\\Database\\Sqlsrv\\SqlsrvStatement',
-            new SqlsrvStatement(static::$driver->getConnection(), $query)
-        );
+		new SqlsrvStatement(static::$connection->getConnection(), $query);
 	}
 
-    /**
-     * Regression test to ensure that named values with matching named params are correctly prepared (part 2).
-     * This simulates a general use case
-     *
-     * @return  void
-     *
-     * @since   __DEPLOY_VERSION
-     */
-    public function testStatementWithKeysMatching()
-    {
-        static::$driver->connect();
-        $query = 'SELECT * FROM dbtest WHERE `id` = :id AND `title` = :id_title';
+	/**
+	 * Regression test to ensure that named values with matching named params are correctly prepared (part 2), this simulates a general use case.
+	 *
+	 * @doesNotPerformAssertions
+	 */
+	public function testStatementWithKeysMatching()
+	{
+		$query = 'SELECT * FROM dbtest WHERE id = :id AND title = :id_title';
 
-        // Dummy assertion to ensure we haven't thrown an exception preparing the statement
-        $this->assertInstanceOf(
-            '\\Joomla\\Database\\Sqlsrv\\SqlsrvStatement',
-            new SqlsrvStatement(static::$driver->getConnection(), $query)
-        );
-    }
+		new SqlsrvStatement(static::$connection->getConnection(), $query);
+	}
 
-    /**
-     * Regression test to ensure that named values with matching named params are correctly prepared (part 3).
-     * This simulates a general use case for a search function where we reuse the same prepared statement term
-     *
-     * @return  void
-     *
-     * @since   __DEPLOY_VERSION
-     */
-    public function testStatementWithMultipleUseOfVars()
-    {
-        static::$driver->connect();
-        $query = 'SELECT * FROM `dbtest` WHERE `description` LIKE :search_term AND `title` LIKE :search_term';
+	/**
+  * Regression test to ensure that named values with matching named params are correctly prepared (part 3).
+  * This simulates a general use case for a search function where we reuse the same prepared statement term.
+  *
+* @doesNotPerformAssertions
+  */
+	public function testStatementWithMultipleUseOfVars()
+	{
+		$query = 'SELECT * FROM dbtest WHERE description LIKE :search_term AND title LIKE :search_term';
 
-        // Dummy assertion to ensure we haven't thrown an exception preparing the statement
-        $this->assertInstanceOf(
-            '\\Joomla\\Database\\Sqlsrv\\SqlsrvStatement',
-            new SqlsrvStatement(static::$driver->getConnection(), $query)
-        );
-    }
+		new SqlsrvStatement(static::$connection->getConnection(), $query);
+	}
 }
