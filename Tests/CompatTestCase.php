f64927908d2e1563eb6449a394f926269f1b23be
Michael Babker <michael.babker@gmail.com>
2020-03-08 11:15:01 -0500

    Move compat classes to separate files to avoid parse errors

--- Tests/CompatTestCase.php
+++ Tests/CompatTestCase.php
@@ -6,81 +6,18 @@
 
 namespace Joomla\Application\Tests;
 
-use PHPUnit\Framework\TestCase;
+use Joomla\Application\Tests\PhpUnit\PhpUnit6TestCase;
+use Joomla\Application\Tests\PhpUnit\PhpUnit7TestCase;
 use PHPUnit\Runner\Version;
 
+// Note, the compatibility classes MUST be in separate files so as to not introduce parse errors for older PHP versions
 if (version_compare(Version::id(), '7.0', '>='))
 {
 	/**
 	 * Compatibility test case used for PHPUnit 7.x and later
 	 */
-	abstract class CompatTestCase extends TestCase
+	abstract class CompatTestCase extends PhpUnit7TestCase
 	{
-		/**
-		 * This method is called before the first test of this test class is run.
-		 */
-		public static function setUpBeforeClass(): void
-		{
-			self::doSetUpBeforeClass();
-		}
-
-		/**
-		 * This method is called after the last test of this test class is run.
-		 */
-		public static function tearDownAfterClass(): void
-		{
-			self::doTearDownAfterClass();
-		}
-
-		/**
-		 * Sets up the fixture, for example, open a network connection.
-		 * This method is called before a test is executed.
-		 */
-		protected function setUp(): void
-		{
-			$this->doSetUp();
-		}
-
-		/**
-		 * Tears down the fixture, for example, close a network connection.
-		 * This method is called after a test is executed.
-		 */
-		protected function tearDown(): void
-		{
-			$this->doTearDown();
-		}
-
-		/**
-		 * Compatibility method for `setUpBeforeClass()`
-		 */
-		protected static function doSetUpBeforeClass()
-		{
-			parent::setUpBeforeClass();
-		}
-
-		/**
-		 * Compatibility method for `tearDownAfterClass()`
-		 */
-		protected static function doTearDownAfterClass()
-		{
-			parent::tearDownAfterClass();
-		}
-
-		/**
-		 * Compatibility method for `setUp()`
-		 */
-		protected function doSetUp()
-		{
-			parent::setUp();
-		}
-
-		/**
-		 * Compatibility method for `tearDown()`
-		 */
-		protected function doTearDown()
-		{
-			parent::tearDown();
-		}
 	}
 }
 else
@@ -88,72 +25,7 @@ else
 	/**
 	 * Compatibility test case used for PHPUnit 6.x and earlier
 	 */
-	abstract class CompatTestCase extends TestCase
+	abstract class CompatTestCase extends PhpUnit6TestCase
 	{
-		/**
-		 * This method is called before the first test of this test class is run.
-		 */
-		public static function setUpBeforeClass()
-		{
-			self::doSetUpBeforeClass();
-		}
-
-		/**
-		 * This method is called after the last test of this test class is run.
-		 */
-		public static function tearDownAfterClass()
-		{
-			self::doTearDownAfterClass();
-		}
-
-		/**
-		 * Sets up the fixture, for example, open a network connection.
-		 * This method is called before a test is executed.
-		 */
-		protected function setUp()
-		{
-			$this->doSetUp();
-		}
-
-		/**
-		 * Tears down the fixture, for example, close a network connection.
-		 * This method is called after a test is executed.
-		 */
-		protected function tearDown()
-		{
-			$this->doTearDown();
-		}
-
-		/**
-		 * Compatibility method for `setUpBeforeClass()`
-		 */
-		protected static function doSetUpBeforeClass()
-		{
-			parent::setUpBeforeClass();
-		}
-
-		/**
-		 * Compatibility method for `tearDownAfterClass()`
-		 */
-		protected static function doTearDownAfterClass()
-		{
-			parent::tearDownAfterClass();
-		}
-
-		/**
-		 * Compatibility method for `setUp()`
-		 */
-		protected function doSetUp()
-		{
-			parent::setUp();
-		}
-
-		/**
-		 * Compatibility method for `tearDown()`
-		 */
-		protected function doTearDown()
-		{
-			parent::tearDown();
-		}
 	}
 }
