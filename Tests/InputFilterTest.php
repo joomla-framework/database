872338c69e1539959d05059d533a483edcc76978
Richard Fath <richard67@users.noreply.github.com>
2021-01-29 15:04:10 +0100

    Fix the test result

--- Tests/InputFilterTest.php
+++ Tests/InputFilterTest.php
@@ -483,7 +483,7 @@ class InputFilterTest extends TestCase
 			'windows path with 2 times /'                                   => array(
 				'path',
 				'C:\\Documents/Newsletters/tmp',
-				'C:\Documents\Newsletters/tmp',
+				'C:\Documents/Newsletters/tmp',
 				'From generic cases'
 			),
 			'user_01'                                                       => array(
