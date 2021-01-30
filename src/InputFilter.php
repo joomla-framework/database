39d8a28dad59f0dd9bd2f261fac5ab6a4269398e
Richard Fath <richard67@users.noreply.github.com>
2021-01-30 09:50:14 +0100

    Fix Windows and Linux pattern

--- src/InputFilter.php
+++ src/InputFilter.php
@@ -1004,14 +1004,14 @@ class InputFilter
 	 */
 	private function cleanPath($source)
 	{
-		$linuxPattern = '/^[A-Za-z0-9_\/-]+[A-Za-z0-9_\.-]*((\\\\\/)+[A-Za-z0-9_-]+[A-Za-z0-9_\.-]*)*$/';
+		$linuxPattern = '/^[A-Za-z0-9_\/-]+[A-Za-z0-9_\.-]*((\\\\|\/)+[A-Za-z0-9_-]+[A-Za-z0-9_\.-]*)*$/';
 
 		if (preg_match($linuxPattern, $source))
 		{
 			return preg_replace('~/+~', '/', $source);
 		}
 
-		$windowsPattern = '/^([A-Za-z]:(\\{1,2}|\/))?[A-Za-z0-9_-]+[A-Za-z0-9_\.-]*((\\{1,2}|\/)[A-Za-z0-9_-]+[A-Za-z0-9_\.-]*)*$/';
+		$windowsPattern = '/^([A-Za-z]:(\\|\\\\|\/))?[A-Za-z0-9_-]+[A-Za-z0-9_\.-]*((\\|\\\\|\/)[A-Za-z0-9_-]+[A-Za-z0-9_\.-]*)*$/';
 
 		if (preg_match($windowsPattern, $source))
 		{
