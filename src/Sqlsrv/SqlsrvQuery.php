82bc3262e4808acc5f4980fa9c497d8a9dd1af21
Phil Taylor <phil@phil-taylor.com>
2021-03-21 17:28:02 +0000

    s/whitepace/whitespace

--- src/Sqlsrv/SqlsrvQuery.php
+++ src/Sqlsrv/SqlsrvQuery.php
@@ -1071,7 +1071,7 @@ class SqlsrvQuery extends DatabaseQuery
 				{
 					if ($i === $start && $colIdx > 0 && !isset($column[$colIdx]))
 					{
-						// Remove whitepace placed before dot
+						// Remove whitespace placed before dot
 						$colIdx--;
 					}
 
@@ -1081,7 +1081,7 @@ class SqlsrvQuery extends DatabaseQuery
 				{
 					if (ctype_space($current))
 					{
-						// Normalize whitepace
+						// Normalize whitespace
 						$string[$i] = ' ';
 
 						if ($start < $i)
