b466d156f334bd10d9f509b99eeddf12f94b305c
Richard Fath <richard67@users.noreply.github.com>
2021-03-24 15:08:42 +0100

    Remove display width from mysql schema test stub

--- Tests/Stubs/Schema/mysql.sql
+++ Tests/Stubs/Schema/mysql.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `#__dbtest` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
   `start_date` datetime NOT NULL,
   `description` text NOT NULL,
