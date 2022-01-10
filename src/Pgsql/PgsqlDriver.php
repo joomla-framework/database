142a624df2f48858467de67a4542d77807cc968b
George Wilson <georgejameswilson@googlemail.com>
2022-01-10 02:28:52 +0000

    Fix order of checks

--- src/Pgsql/PgsqlDriver.php
+++ src/Pgsql/PgsqlDriver.php
@@ -271,16 +271,16 @@ class PgsqlDriver extends PdoDriver
 			{
 				if ($field->Default !== null)
 				{
-					// Change Postgresql's NULL::* type with PHP's null one
-					if (preg_match('/^NULL::*/', $field->Default))
-					{
-						$field->Default = null;
-					}
-
 					// Normalise default values like datetime
 					if (preg_match('/^\'(.*)\'::.*/', $field->Default, $matches))
 					{
 						$field->Default = $matches[1];
+					}
+
+					// Change Postgresql's NULL::* type with PHP's null one. Do this last to avoid PHP type issues in PHP 8.1 and up
+					if (preg_match('/^NULL::*/', $field->Default))
+					{
+						$field->Default = null;
 					}
 				}
 
