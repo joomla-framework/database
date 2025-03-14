f4bdb4ff3aac6403b43353b9d535699e3dc018c5
Richard Fath <richard.fath@t-online.de>
2025-03-14 10:19:19 +0100

    Add collation to getAlterDbCharacterSet

--- src/Mysqli/MysqliDriver.php
+++ src/Mysqli/MysqliDriver.php
@@ -392,9 +392,10 @@ class MysqliDriver extends DatabaseDriver implements UTF8MB4SupportInterface
      */
     public function getAlterDbCharacterSet($dbName)
     {
-        $charset = $this->utf8mb4 ? 'utf8mb4' : 'utf8';
+        $charset   = $this->utf8mb4 ? 'utf8mb4' : 'utf8';
+        $collation = $charset . '_unicode_ci';
 
-        return 'ALTER DATABASE ' . $this->quoteName($dbName) . ' CHARACTER SET `' . $charset . '`';
+        return 'ALTER DATABASE ' . $this->quoteName($dbName) . ' CHARACTER SET `' . $charset . '` COLLATE `' . $collation . '`';
     }
 
     /**
