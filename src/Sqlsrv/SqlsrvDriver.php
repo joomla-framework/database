bbcc3a423fb3ad4ce26ce9fea1d0f152e5c139c7
Richard Fath <richard.fath@t-online.de>
2025-03-22 13:58:32 +0100

    Cast to bool as for other boolean options

--- src/Sqlsrv/SqlsrvDriver.php
+++ src/Sqlsrv/SqlsrvDriver.php
@@ -87,7 +87,7 @@ class SqlsrvDriver extends DatabaseDriver
 		$options['password'] = $options['password'] ?? '';
 		$options['database'] = $options['database'] ?? '';
 		$options['select']   = isset($options['select']) ? (bool) $options['select'] : true;
-		$options['trust_certificate'] = $options['trust_certificate'] ?? false;
+		$options['trust_certificate'] = isset($options['trust_certificate']) ? (bool) $options['trust_certificate'] : false;
 
 		// Finalize initialisation
 		parent::__construct($options);
