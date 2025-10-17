9a2aec5838de2939c2f85d54eacf0f5ed30ec373
Kamil Tekiela <tekiela246@gmail.com>
2025-10-17 14:52:20 +0100

    Add mysqli exception mode

--- src/Mysqli/MysqliStatement.php
+++ src/Mysqli/MysqliStatement.php
@@ -135,10 +135,10 @@ class MysqliStatement implements StatementInterface
 
         $query = $this->prepareParameterKeyMapping($query);
 
-        $this->statement  = $connection->prepare($query);
-
-        if (!$this->statement) {
-            throw new PrepareStatementFailureException($this->connection->error, $this->connection->errno);
+        try {
+            $this->statement = $connection->prepare($query);
+        } catch (\mysqli_sql_exception $e) {
+            throw new PrepareStatementFailureException($e->getMessage(), $e->getCode(), $e);
         }
     }
 
@@ -394,19 +394,21 @@ class MysqliStatement implements StatementInterface
 
             array_unshift($params, implode('', $types));
 
-            if (!\call_user_func_array([$this->statement, 'bind_param'], $params)) {
-                throw new PrepareStatementFailureException($this->statement->error, $this->statement->errno);
+            try {
+                \call_user_func_array([$this->statement, 'bind_param'], $params);
+            } catch (\Exception $e) {
+                throw new PrepareStatementFailureException($e->getMessage(), $e->getCode(), $e);
             }
         } elseif ($parameters !== null) {
-            if (!$this->bindValues($parameters)) {
-                throw new PrepareStatementFailureException($this->statement->error, $this->statement->errno);
+            try {
+                $this->bindValues($parameters);
+            } catch (\Exception $e) {
+                throw new PrepareStatementFailureException($e->getMessage(), $e->getCode(), $e);
             }
         }
 
         try {
-            if (!$this->statement->execute()) {
-                throw new ExecutionFailureException($this->query, $this->statement->error, $this->statement->errno);
-            }
+            $this->statement->execute();
         } catch (\Throwable $e) {
             throw new ExecutionFailureException($this->query, $e->getMessage(), $e->getCode(), $e);
         }
@@ -439,9 +441,7 @@ class MysqliStatement implements StatementInterface
                 $refs[$key] =& $value;
             }
 
-            if (!\call_user_func_array([$this->statement, 'bind_result'], $refs)) {
-                throw new \RuntimeException($this->statement->error, $this->statement->errno);
-            }
+            \call_user_func_array([$this->statement, 'bind_result'], $refs);
         }
 
         $this->result = true;
@@ -483,10 +483,6 @@ class MysqliStatement implements StatementInterface
 
         if ($values === null) {
             return false;
-        }
-
-        if ($values === false) {
-            throw new \RuntimeException($this->statement->error, $this->statement->errno);
         }
 
         switch ($fetchStyle) {
@@ -540,7 +536,11 @@ class MysqliStatement implements StatementInterface
      */
     private function fetchData()
     {
-        $return = $this->statement->fetch();
+        try {
+            $return = $this->statement->fetch();
+        } catch (\Throwable $e) {
+            throw new \RuntimeException($e->getMessage(), $e->getCode(), $e);
+        }
 
         if ($return === true) {
             $values = [];
