fe9d3da538fe54f5912bdd0726418bcff0de30ff
Christian Heel <66922325+heelc29@users.noreply.github.com>
2026-07-06 18:04:41 +0200

    set trust_certificate for sqlsrv

--- Tests/Sqlsrv/SqlsrvDriverTest.php
+++ Tests/Sqlsrv/SqlsrvDriverTest.php
@@ -12,6 +12,7 @@ use Joomla\Database\Exception\UnsupportedAdapterException;
 use Joomla\Database\Sqlsrv\SqlsrvDriver;
 use Joomla\Database\Sqlsrv\SqlsrvQuery;
 use Joomla\Database\Tests\AbstractDatabaseDriverTestCase;
+use Joomla\Test\DatabaseManager;
 use PHPUnit\Framework\Attributes\DataProvider;
 
 /**
@@ -19,6 +20,22 @@ use PHPUnit\Framework\Attributes\DataProvider;
  */
 class SqlsrvDriverTest extends AbstractDatabaseDriverTestCase
 {
+    protected static function createDatabaseManager(): DatabaseManager
+    {
+        $manager = new class () extends DatabaseManager {
+            protected function initialiseParams(): void
+            {
+                parent::initialiseParams();
+
+                if (\is_array($this->params)) {
+                    $this->params['trust_certificate'] = true;
+                }
+            }
+        };
+
+        return $manager;
+    }
+
     /**
      * This method is called before the first test of this test class is run.
      *
